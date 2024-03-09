6d0d123dcbff71e89b18d59456e0a09f28b4ad77
Candelaria Barcelo <barcelo.candelaria@gmail.com>
2024-03-09 16:30:52 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function(e) {
+//         e.preventDefault();
+
+//         const section = document.querySelector(this.getAttribute('href'));
+
+//         window.scrollTo({
+//             top: section.offsetTop,
+//             behavior: 'smooth'
+//         });
+//     });
+// });
